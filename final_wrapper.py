be70e8631ea0d872fc6537cae0a6cc42d0e9c251
jaya kumar <101202728+jayakumarramisetty@users.noreply.github.com>
2022-06-14 12:57:37 +0530

    Update final_wrapper.py

--- final_wrapper.py
+++ final_wrapper.py
@@ -2,59 +2,57 @@ import json
 import io
 import yaml
 import json
-
 import importlib.util
 spec = importlib.util.spec_from_file_location("module.name", "/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/ipaddress.py")
 ipaddress = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(ipaddress)
-
+#User provided input for both #of rules and #of policies
+x=input("Enter the number of rules you need per policy: \n")
+x=int(x)
+y=input("Enter the number of polocies: \n")
+y=int(y)
+#define main dict
 policy4 = {"Security_policy":[]}
-
-for n in range(2):
+#1st loop start for security policy body iteration
+for n in range(y):
     policies = {
       "name": "netsecpolicy"+str(n),
       "rules": [],
       "priority": ""
     }
   
-
+#define the IP address for iteration
     sip="10."+str(n)+"."+"0."+"1"
     dip="11."+str(n)+"."+"0."+"1"
     from_ip = int(ipaddress.IPv4Address(str(sip)))
     to_ip = int(ipaddress.IPv4Address(str(dip)))
     rule = {}
 
-
-    for i in range(60):
+#2nd loop for the rule body iteration
+    for i in range(x):
         rule["name"] = "rule_{}".format(i)
         rule["description"] = "rule_description_{}".format(i)
         rule["action"] = "permit"
         rule["from-ip-addresses"] = [str(ipaddress.IPv4Address(from_ip + i))]
         rule["to-ip-addresses"] = [str(ipaddress.IPv4Address(to_ip + i))]
         rule["proto-ports"] = [ { "protocol": "tcp", "ports": "443,80" } ]
+        #define the rule dictionary
         rule1={}
-    
         rule1.update(rule)
-
-
-        
         policies["rules"].append(rule1)
+        #define policy dictionary
         policy1={}
         policy1.update(policies)
 
-    
+    #append the output to the final dict
     policy4["Security_policy"].append(policy1)
 
-    
-
-
-    
-
+#print the required output
 print(json.dumps(policy4)) 
 # Write json to YAML file
-with open(r'/Users/jaikumar/Documents/data5.yaml', 'a') as f:
+with open(r'/Users/jaikumar/Documents/data_input_rules.yaml', 'w') as f:
         yaml.dump(policy4, f, default_flow_style=False, sort_keys=False)
-
+#print the type of output and it is dictionary
 print(type(policy4))
 
 
